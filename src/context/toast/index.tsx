b939852ec3c29d7d1dcc68e5c3b3a9e08e60e049
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-26 19:30:09 +0330

    fix(toast): remove initialized toast

--- src/context/toast/index.tsx
+++ src/context/toast/index.tsx
@@ -1,28 +1,23 @@
-import { useEffect } from "react";
-import { Toaster, toast } from "sonner";
+import { Toaster } from "sonner";
 
-export const ToastProvider = () => {
-  useEffect(() => {
-    toast.success("toast system initialized");
-  }, []);
-  return (
-    <Toaster
-      closeButton
-      richColors
-      position="top-center"
-      expand
-      duration={4000}
-      visibleToasts={5}
-      toastOptions={{
-        classNames: {
-          toast: "border p-4 rounded-lg shadow-lg text-sm font-medium",
-          title: "font-semibold text-base mb-1",
-          description: "text-muted-foreground",
-          cancelButton: "text-xs text-gray-500 hover:text-gray-700",
-          actionButton: "bg-blue-600 text-white text-xs px-2 py-1 rounded hover:bg-blue-700",
-          closeButton: "text-xl hover:text-red-500 transition-all",
-        },
-      }}
-    />
-  );
-};
+export const ToastProvider = () => (
+  <Toaster
+    closeButton
+    richColors
+    position="top-center"
+    expand
+    duration={4000}
+    visibleToasts={5}
+    toastOptions={{
+      classNames: {
+        toast: "border p-4 rounded-lg shadow-lg text-sm font-medium",
+        title: "font-semibold text-base mb-1",
+        description: "text-muted-foreground",
+        cancelButton: "text-xs text-gray-500 hover:text-gray-700",
+        actionButton:
+          "bg-blue-600 text-white text-xs px-2 py-1 rounded hover:bg-blue-700",
+        closeButton: "text-xl hover:text-red-500 transition-all",
+      },
+    }}
+  />
+);
