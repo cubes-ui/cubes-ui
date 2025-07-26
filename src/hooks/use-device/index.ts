3e2d5264035681d51ecf2998df50d321500a156f
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-26 03:50:51 +0330

    feat(use-device): add use-device

---
+++ src/hooks/use-device/index.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect, useCallback } from "react";
+import { isBrowser } from "../../utils";
+
+type DeviceState = "Mobile" | "Tablet" | "Desktop";
+
+interface DeviceInfo {
+  state: DeviceState;
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+}
+const getDeviceType = (): DeviceState => {
+  if (
+    !(typeof window !== "undefined" && typeof window.document !== "undefined")
+  )
+    return "Desktop";
+
+  const width = window.innerWidth;
+
+  if (width < 768) return "Mobile";
+  if (width < 992) return "Tablet";
+  return "Desktop";
+};
+
+const useDevice = (): DeviceInfo => {
+  const [state, setState] = useState<DeviceState>(() => getDeviceType());
+
+  const updateDeviceState = useCallback(() => {
+    setState(getDeviceType());
+  }, []);
+
+  useEffect(() => {
+    if (!isBrowser) return;
+
+    window.addEventListener("resize", updateDeviceState);
+    return () => window.removeEventListener("resize", updateDeviceState);
+  }, [updateDeviceState]);
+
+  return {
+    state,
+    isMobile: state === "Mobile",
+    isTablet: state === "Tablet",
+    isDesktop: state === "Desktop",
+  };
+};
+
+export default useDevice;
