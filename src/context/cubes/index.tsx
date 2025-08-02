3bc415e2edeb606aee272296880223717491bbed
Alisan <99.a.cpcf.r.99@gmail.com>
2025-08-02 19:35:54 +0330

    chore(contexts): remove unused imported libs

--- src/context/cubes/index.tsx
+++ src/context/cubes/index.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 import { env } from "../../utils/env";
 import { ToastProvider } from "../toast";
 
